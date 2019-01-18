70bb01f259f7baff61c75c3775be000abf00bd39
piotrkaminski95 <pkaminki95@gmail.com>
2019-01-18 01:27:22 +0100

    add Timer

---
+++ src/main/java/com/codecool/Timer.java
@@ -0,0 +1,13 @@
+package com.codecool;
+
+public class Timer {
+    private int time = 0;
+
+    public void increment() {
+        this.time++;
+    }
+
+    public int getTime() {
+        return this.time;
+    }
+}
