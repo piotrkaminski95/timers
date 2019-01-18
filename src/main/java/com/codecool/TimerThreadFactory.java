b0d9552bf96d16a3da844d434ad7782a4928d933
piotrkaminski95 <pkaminki95@gmail.com>
2019-01-18 01:41:12 +0100

    implement TimerThreadFactory

--- src/main/java/com/codecool/TimerThreadFactory.java
+++ src/main/java/com/codecool/TimerThreadFactory.java
@@ -1,4 +1,9 @@
 package com.codecool;
 
 public class TimerThreadFactory {
+    private static int counter = 0;
+
+    public TimerThread getTimerThread(String name) {
+        return new TimerThread(++counter, name);
+    }
 }
