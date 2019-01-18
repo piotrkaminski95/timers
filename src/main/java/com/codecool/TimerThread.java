0783a4645132966709000b64632bc9cc7cf4d16e
piotrkaminski95 <pkaminki95@gmail.com>
2019-01-18 01:32:22 +0100

    add constructor and toString()

--- src/main/java/com/codecool/TimerThread.java
+++ src/main/java/com/codecool/TimerThread.java
@@ -1,6 +1,21 @@
 package com.codecool;
 
 public class TimerThread extends Thread {
+    private Timer timer;
+    private int id;
+    private String name;
+
+    public TimerThread(int id, String name) {
+        this.timer = new Timer();
+        this.id = id;
+        this.name = name;
+    }
+
     @Override
     public void run() {}
+
+    @Override
+    public String toString() {
+        return String.format("Name: %s, ThreadId: %d, Seconds: %d", name, id, timer.getTime());
+    }
 }
